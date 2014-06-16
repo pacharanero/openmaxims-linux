35aa172a9f3223107c9d87b97107da97919eb2e8
pacharanero <marcusbaw@gmail.com>
2014-06-16 06:52:46 +0000

    first commit - tweaks as per README.md

---
+++ build_openMAXIMS.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+cd ./openmaxims_workspace/WebApp
+ant -f WebAppBuild.xml
+
---
+++ setenv.bat
@@ -0,0 +1,2 @@
+set PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/java/jdk/jdk1.5.0_22/bin:/apache-ant-1.6.5/
+set ANT_OPTS="-Xmx1024m"
